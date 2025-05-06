a564ad3015d7ec473e094b6ddb7622b9c6cb94ac
Ed J <mohawk2@users.noreply.github.com>
2025-05-06 03:12:39 +0100

    ::GLUT no need package-qualify OpenGL::GLUT stuff since import

--- lib/PDL/Graphics/TriD/GL/GLUT.pm
+++ lib/PDL/Graphics/TriD/GL/GLUT.pm
@@ -22,32 +22,32 @@ sub new {
 sub _init_glut_window {
   my ($self, $window_obj) = @_;
   my $p = $self->{Options};
-  OpenGL::GLUT::glutInitWindowPosition( $p->{x}, $p->{y} );
-  OpenGL::GLUT::glutInitWindowSize( $p->{width}, $p->{height} );
-  OpenGL::GLUT::glutInitDisplayMode( OpenGL::GLUT::GLUT_RGBA() | OpenGL::GLUT::GLUT_DOUBLE() | OpenGL::GLUT::GLUT_DEPTH() );        # hardwire for now
+  glutInitWindowPosition( $p->{x}, $p->{y} );
+  glutInitWindowSize( $p->{width}, $p->{height} );
+  glutInitDisplayMode( GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH ); # hardwire for now
   if ($^O ne 'MSWin32' and not $OpenGL::Config->{DEFINE} =~ /-DHAVE_W32API/) { # skip these MODE checks on win32, they don't work
-     if (not OpenGL::GLUT::glutGet(OpenGL::GLUT::GLUT_DISPLAY_MODE_POSSIBLE()))
+     if (not glutGet(GLUT_DISPLAY_MODE_POSSIBLE))
      {
         warn "glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH | GLUT_ALPHA) not possible";
         warn "...trying without GLUT_ALPHA";
         # try without GLUT_ALPHA
-        OpenGL::GLUT::glutInitDisplayMode( OpenGL::GLUT::GLUT_RGBA() | OpenGL::GLUT::GLUT_DOUBLE() | OpenGL::GLUT::GLUT_DEPTH() );
-        if ( not OpenGL::GLUT::glutGet( OpenGL::GLUT::GLUT_DISPLAY_MODE_POSSIBLE() ) )
+        glutInitDisplayMode( GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH );
+        if ( not glutGet( GLUT_DISPLAY_MODE_POSSIBLE ) )
         {
            die "display mode not possible";
         }
      }
   }
-  $self->{glutwindow} = OpenGL::GLUT::glutCreateWindow( "GLUT TriD" );
-  OpenGL::GLUT::glutSetWindowTitle("GLUT TriD #$self->{glutwindow}");
-  OpenGL::GLUT::glutReshapeFunc( \&_pdl_fake_ConfigureNotify );
-  OpenGL::GLUT::glutCloseFunc( \&_pdl_fake_exit_handler );
-  OpenGL::GLUT::glutKeyboardFunc( \&_pdl_fake_KeyPress );
-  OpenGL::GLUT::glutMouseFunc( \&_pdl_fake_button_event );
-  OpenGL::GLUT::glutMotionFunc( \&_pdl_fake_MotionNotify );
-  OpenGL::GLUT::glutDisplayFunc( \&_pdl_display_wrapper );
-  OpenGL::GLUT::glutSetOption(OpenGL::GLUT::GLUT_ACTION_ON_WINDOW_CLOSE(), OpenGL::GLUT::GLUT_ACTION_GLUTMAINLOOP_RETURNS()) if OpenGL::GLUT::_have_freeglut();
-  OpenGL::GLUT::glutMainLoopEvent();       # pump event loop so window appears
+  $self->{glutwindow} = glutCreateWindow( "GLUT TriD" );
+  glutSetWindowTitle("GLUT TriD #$self->{glutwindow}");
+  glutReshapeFunc( \&_pdl_fake_ConfigureNotify );
+  glutCloseFunc( \&_pdl_fake_exit_handler );
+  glutKeyboardFunc( \&_pdl_fake_KeyPress );
+  glutMouseFunc( \&_pdl_fake_button_event );
+  glutMotionFunc( \&_pdl_fake_MotionNotify );
+  glutDisplayFunc( \&_pdl_display_wrapper );
+  glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS) if _have_freeglut();
+  glutMainLoopEvent();       # pump event loop so window appears
   if ($PDL::Graphics::TriD::verbose) {
     print "gdriver: Got TriD::GL object(GLUT window ID# " . $self->{glutwindow} . ")\n";
   }
@@ -56,28 +56,28 @@ sub _init_glut_window {
 
 sub DESTROY {
   my ($self) = @_;
-  return if !OpenGL::GLUT::done_glutInit();
-  print __PACKAGE__."::DESTROY called (win=$self->{glutwindow}), GLUT says ", OpenGL::GLUT::glutGetWindow(), "\n" if $PDL::Graphics::TriD::verbose;
-  OpenGL::GLUT::glutMainLoopEvent(); # pump to deal with any clicking "X"
-  if (!OpenGL::GLUT::glutGetWindow()) {
+  return if !done_glutInit();
+  print __PACKAGE__."::DESTROY called (win=$self->{glutwindow}), GLUT says ", glutGetWindow(), "\n" if $PDL::Graphics::TriD::verbose;
+  glutMainLoopEvent(); # pump to deal with any clicking "X"
+  if (!glutGetWindow()) {
     # "X" was clicked, clear queue then stop
     @{ $self->{xevents} } = ();
-    OpenGL::GLUT::glutMainLoopEvent(); # pump once
+    glutMainLoopEvent(); # pump once
     return;
   }
-  OpenGL::GLUT::glutSetWindow($self->{glutwindow});
-  OpenGL::GLUT::glutReshapeFunc();
-  OpenGL::GLUT::glutCloseFunc();
-  OpenGL::GLUT::glutKeyboardFunc();
-  OpenGL::GLUT::glutMouseFunc();
-  OpenGL::GLUT::glutMotionFunc();
-  OpenGL::GLUT::glutDestroyWindow($self->{glutwindow});
-  OpenGL::GLUT::glutMainLoopEvent() for 1..2; # pump so window gets actually closed
+  glutSetWindow($self->{glutwindow});
+  glutReshapeFunc();
+  glutCloseFunc();
+  glutKeyboardFunc();
+  glutMouseFunc();
+  glutMotionFunc();
+  glutDestroyWindow($self->{glutwindow});
+  glutMainLoopEvent() for 1..2; # pump so window gets actually closed
   delete $self->{glutwindow};
 }
 
 sub _pdl_display_wrapper {
-   my ($win) = OpenGL::GLUT::glutGetWindow();
+   my ($win) = glutGetWindow();
    if ( defined($win) and defined($winObjects[$win]) ) {
       $winObjects[$win]->display();
    }
@@ -91,7 +91,7 @@ sub _pdl_fake_exit_handler {
 
 sub _pdl_fake_ConfigureNotify {
    print "_pdl_fake_ConfigureNotify: got (@_)\n" if $PDL::Graphics::TriD::verbose;
-   OpenGL::GLUT::glutPostRedisplay();
+   glutPostRedisplay();
    push @fakeXEvents, [ 'reshape', @_ ];
 }
 
@@ -138,7 +138,7 @@ sub _pdl_fake_KeyPress {
 sub event_pending {
    my($self) = @_;
    # monitor state of @fakeXEvents, return number on queue
-   OpenGL::GLUT::glutMainLoopEvent() if !@{$self->{xevents}};
+   glutMainLoopEvent() if !@{$self->{xevents}};
    print STDERR "OO::event_pending: have " .  scalar( @{$self->{xevents}} ) . " xevents\n" if $PDL::Graphics::TriD::verbose > 1;
    scalar( @{$self->{xevents}} );
 }
@@ -147,7 +147,7 @@ sub next_event {
   my($self) = @_;
   while ( !scalar( @{$self->{xevents}} ) ) {
     # If no events, we keep pumping the event loop
-    OpenGL::GLUT::glutMainLoopEvent();
+    glutMainLoopEvent();
   }
   # Extract first event from fake event queue and return
   @{ shift @{$self->{xevents}} };
@@ -155,13 +155,13 @@ sub next_event {
 
 sub swap_buffers {
   my ($this) = @_;
-  OpenGL::GLUT::glutSwapBuffers();
+  glutSwapBuffers();
 }
 
 sub set_window {
   my ($this) = @_;
   # set GLUT context to current window (for multiwindow support)
-  OpenGL::GLUT::glutSetWindow($this->{glutwindow});
+  glutSetWindow($this->{glutwindow});
 }
 
 1;
