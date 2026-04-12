5f7d1986a706882df6ba2e4e34bcd55269cdde10
Ed J <mohawk2@users.noreply.github.com>
2026-04-12 19:42:53 +0100

    stack-trace if add_scale gets undef $inds

--- lib/PDL/Graphics/TriD/Graph.pm
+++ lib/PDL/Graphics/TriD/Graph.pm
@@ -63,9 +63,9 @@ sub bind_default {
 }
 
 sub set_axis {
-	my($this,$axis,$name) = @_;
-	$this->{Axis}{$name} = $axis;
-	$this->changed();
+  my ($this,$axis,$name) = @_;
+  $this->{Axis}{$name} = $axis;
+  $this->changed();
 }
 
 # Bind all unbound things here...
@@ -99,15 +99,15 @@ sub get_points {
 }
 
 sub clear_data {
-	my($this) = @_;
-	$this->{$_} = {} for qw(Data DataBind UnBound);
-	$this->changed;
+  my ($this) = @_;
+  $this->{$_} = {} for qw(Data DataBind UnBound);
+  $this->changed;
 }
 
 sub delete_data {
-	my($this,$name) = @_;
-	delete $this->{$_}{$name} for qw(Data DataBind UnBound);
-	$this->changed;
+  my ($this,$name) = @_;
+  delete $this->{$_}{$name} for qw(Data DataBind UnBound);
+  $this->changed;
 }
 
 use constant { DEFAULT_AXIS=>'Euclid3', DEFAULT_AXIS_CLASS=>'PDL::Graphics::TriD::EuclidAxes', DEFAULT_INDICES=>[0,1,2] };
@@ -119,9 +119,9 @@ sub default_axes {
 }
 
 sub set_default_axis {
-	my($this,$name,$axes) = @_;
-	$this->{DefaultAxisName} = $name;
-	$this->{DefaultAxes} = $axes;
+  my ($this,$name,$axes) = @_;
+  $this->{DefaultAxisName} = $name;
+  $this->{DefaultAxes} = $axes;
 }
 
 sub changed {}
@@ -156,12 +156,13 @@ sub new {
 }
 
 sub init_scale {
-  my($this) = @_;
+  my ($this) = @_;
   $this->{Scale} = undef;
 }
 
 sub add_scale {
-  my($this,$data,$inds) = @_;
+  my ($this,$data,$inds) = @_;
+  PDL::barf "no \$inds given" if !defined $inds;
   $data = $data->dice_axis(0, $inds);
   my $to_minmax = $data->clump(1..$data->ndims-1); # xyz,...
   $to_minmax = $to_minmax->glue(1, $this->{Scale}); # include old min/max
@@ -170,7 +171,7 @@ sub add_scale {
 }
 
 sub finish_scale {
-  my($this) = @_;
+  my ($this) = @_;
 # Normalize the smallest differences away.
   my ($min, $max) = $this->{Scale}->dog;
   my $diff = $max - $min;
@@ -185,7 +186,7 @@ sub finish_scale {
 
 # Add 0..1 to each axis.
 sub transform {
-  my($this,$point,$data,$inds) = @_;
+  my ($this,$point,$data,$inds) = @_;
   my ($min, $max) = map $this->{Scale}->slice("0:$#$inds,$_"), 0, 1;
   $point->slice("0:$#$inds") +=
     ($data->dice_axis(0, $inds) - $min) / ($max - $min);
@@ -205,17 +206,18 @@ use fields qw/Name Scale/;
 use PDL::Core '';
 
 sub new {
-	my($type) = @_; 
-	bless {Names => [qw(LON LAT Pressure)]},$type;
+  my ($type) = @_;
+  bless {Names => [qw(LON LAT Pressure)]},$type;
 }
 
 sub init_scale {
-	my($this) = @_;
-	$this->{Scale} = [];
+  my ($this) = @_;
+  $this->{Scale} = [];
 }
 
 sub add_scale {
-  my($this,$data,$inds) = @_;
+  my ($this,$data,$inds) = @_;
+  PDL::barf "no \$inds given" if !defined $inds;
   my $i = 0;
   for (@$inds) {
     my $d = $data->slice("($_)");
@@ -244,17 +246,17 @@ sub add_scale {
   }
 # Should make the projection center an option
   $this->{Center} = [$this->{Scale}[0][0]+($this->{Scale}[0][1]-$this->{Scale}[0][0])/2,
-		     0];
+     0];
 }
 
 sub finish_scale {
-  my($this) = @_;
+  my ($this) = @_;
   my @dist;
   # Normalize the smallest differences away.
   for (@{$this->{Scale}}) {
     if (abs($_->[0] - $_->[1]) < 0.000001) {
       $_->[1] = $_->[0] + 1;
-    } 
+    }
     push(@dist,$_->[1]-$_->[0]);
   }
   # for the z coordinate reverse the min and max values
@@ -273,7 +275,7 @@ sub finish_scale {
   } elsif ($dist[0] > $dist[1]) {
     $this->{Scale}[1][0] -= (90-$dist[1])/2;
     $this->{Scale}[1][1] += (90-$dist[1])/2;
-  }    
+  }
   my (@nadd,@nc,@ns);
   for my $dim (0..1) {
     my $width = $this->{Scale}[$dim][1]-$this->{Scale}[$dim][0];
@@ -300,7 +302,7 @@ sub finish_scale {
 }
 
 sub transform {
-  my($this,$point,$data,$inds) = @_;
+  my ($this,$point,$data,$inds) = @_;
   barf "Wrong number of arguments to transform $this\n" if @$inds != 3;
   my $i = 0;
   my $pio180 = 0.017453292;
@@ -320,17 +322,18 @@ package PDL::Graphics::TriD::PolarStereoAxes;
 use PDL::Core '';
 
 sub new {
-	my($type) = @_; 
-	bless {Names => [qw(LONGITUDE LATITUDE HEIGHT)]},$type;
+  my ($type) = @_;
+  bless {Names => [qw(LONGITUDE LATITUDE HEIGHT)]},$type;
 }
 
 sub init_scale {
-	my($this) = @_;
-	$this->{Scale} = [];
+  my ($this) = @_;
+  $this->{Scale} = [];
 }
 
 sub add_scale {
-  my($this,$data,$inds) = @_;
+  my ($this,$data,$inds) = @_;
+  PDL::barf "no \$inds given" if !defined $inds;
   my $i = 0;
   for (@$inds) {
     my $d = $data->slice("($_)");
@@ -358,17 +361,17 @@ sub add_scale {
     $i++;
   }
   $this->{Center} = [$this->{Scale}[0][0]+($this->{Scale}[0][1]-$this->{Scale}[0][0])/2,
-		     $this->{Scale}[1][0]+($this->{Scale}[1][1]-$this->{Scale}[1][0])/2];
+     $this->{Scale}[1][0]+($this->{Scale}[1][1]-$this->{Scale}[1][0])/2];
 }
 
 sub finish_scale {
-  my($this) = @_;
+  my ($this) = @_;
   my @dist;
   # Normalize the smallest differences away.
   for (@{$this->{Scale}}) {
     if (abs($_->[0] - $_->[1]) < 0.000001) {
       $_->[1] = $_->[0] + 1;
-    } 
+    }
     push(@dist,$_->[1]-$_->[0]);
   }
   # for the z coordinate reverse the min and max values
@@ -387,11 +390,11 @@ sub finish_scale {
   } elsif ($dist[0] > $dist[1]) {
     $this->{Scale}[1][0] -= (90-$dist[1])/2;
     $this->{Scale}[1][1] += (90-$dist[1])/2;
-  }    
+  }
 }
 
 sub transform {
-  my($this,$point,$data,$inds) = @_;
+  my ($this,$point,$data,$inds) = @_;
   my $i = 0;
   barf "Wrong number of arguments to transform $this\n" if @$inds != 3;
   my $pio180 = 0.017453292;
